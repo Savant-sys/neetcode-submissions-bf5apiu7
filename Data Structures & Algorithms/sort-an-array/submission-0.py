429db03fb4c0fb8bb2d81d25fd10f00cdd37237c
Michael Khuri <michaelkhuri@gmail.com>
2026-03-31 23:37:22 -0700

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        swapped = True
+
+        while swapped:
+            swapped = False
+            for i in range(len(nums) - 1):
+                if nums[i] > nums[i + 1]:
+                    nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                    swapped = True
+
+        return nums
