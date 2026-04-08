8fb29c2ec4d0ca5534856ce08bda52c2b2894c83
Michael Khuri <michaelkhuri@gmail.com>
2026-04-08 14:38:27 -0700

    Add: sqrtx - submission-1

---
+++ Data Structures & Algorithms/sqrtx/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l = 0
+        r = x
+        res = 0
+
+        while l <= r:
+            m = l + ((r - l) // 2)
+
+            if m**2 > x:
+                r = m - 1 # 3
+            elif m**2 < x:
+                l = m + 1 # 2, 3
+                res = m
+            else:
+                return m
+        return res
