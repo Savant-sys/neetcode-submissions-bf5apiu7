f45e90f5f40cd4c9c732f365960107de69450a7c
Michael Khuri <michaelkhuri@gmail.com>
2026-05-29 21:16:36 -0700

    Add: is-subsequence - submission-3

---
+++ Data Structures & Algorithms/is-subsequence/submission-3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if s == "":
+            return True
+
+        i = 0
+
+        for c in t:
+            if i < len(s) and s[i] == c:
+                i += 1
+
+        return i == len(s)
